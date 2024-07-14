26c96d2154bad429e7bdc9308f6e07fd0ce571ec
sumit212-dev <132038818+sumit2221-div@users.noreply.github.com>
2024-07-14 11:02:08 +0530

    added

---
+++ script.js
@@ -0,0 +1,8 @@
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+  }
+  
